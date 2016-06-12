428d7d9637e292c70d8e71518e1d28e9783e928e
Pheothenum <zero2004hr@gmail.com>
2016-06-12 17:51:42 +0200

    Dodani aktivity za 3 tipa pitanja

---
+++ app/src/main/java/hr/unipu/polyling/quizListenActivity.java
@@ -0,0 +1,13 @@
+package hr.unipu.polyling;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class quizListenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_listen);
+    }
+}
