73d240656405cbd484ea3cbe627faf418920ce34
lpican <lea.pican@gmail.com>
2016-06-13 01:38:42 +0200

    evo greška

--- app/src/main/java/hr/unipu/polyling/rezultatiActivity.java
+++ app/src/main/java/hr/unipu/polyling/rezultatiActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class rezultatiActivity extends AppCompatActivity {
 
@@ -12,7 +13,30 @@ public class rezultatiActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rezultati);
 
-//        int brojTocnih = getIntent().getIntExtra("brojTocnih");
+        int brojTocnih = getIntent().getIntExtra("brojTocnih", 0);
+        String poruka = null;
+         switch (brojTocnih) {
+                 case 0: poruka="Very bad";
+                     break;
+                 case 1: poruka="Bad!";
+                    break;
+                 case 2: poruka="Not so good!";
+                     break;
+                 case 3: poruka="Good job!";
+                     break;
+                 case 4: poruka="Great!";
+                     break;
+                 case 5: poruka="Exellent!";
+                     break;
+
+         }
+
+        TextView viewBrojTocnih = (TextView) findViewById(R.id.brojTocnih);
+        TextView viewPoruka = (TextView) findViewById(R.id.poruka);
+
+        viewBrojTocnih.setText(brojTocnih);
+        viewPoruka.setText(poruka);
+
 
 
     }
