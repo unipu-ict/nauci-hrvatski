6ee6d9140f705849d12b75559a9d3af37affdf57
Alen Duda <alenduda@gmail.com>
2016-06-13 02:25:25 +0200

    Osposobljen odabir vrste pitanja za kviz

--- app/src/main/java/hr/unipu/polyling/QuizActivity.java
+++ app/src/main/java/hr/unipu/polyling/QuizActivity.java
@@ -3,6 +3,7 @@ package hr.unipu.polyling;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.util.Random;
 
@@ -17,7 +18,8 @@ public class QuizActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(getResources().getString(R.string.quiz_caption));
 
         kategorijaID = getIntent().getIntExtra("kategorijaID", 0);
-        getRandomQuiz();
+
+
     }
 
     private void getRandomQuiz() {
@@ -34,5 +36,23 @@ public class QuizActivity extends AppCompatActivity {
         intent.putExtra("kategorijaID", kategorijaID);
         startActivity(intent);
         finish();
+    }
+
+    public void otvoriListen(View view) {
+
+        Intent intent = new Intent(this, quizListenActivity.class);
+        intent.putExtra("kategorijaID", kategorijaID);
+        startActivity(intent);
+        finish();
+
+    }
+
+    public void otvoriChoice(View view) {
+
+        Intent intent = new Intent(this, quizChoiceActivity.class);
+        intent.putExtra("kategorijaID", kategorijaID);
+        startActivity(intent);
+        finish();
+
     }
 }
