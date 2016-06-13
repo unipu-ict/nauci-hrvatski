74018a1ad8f5ca8d551373393cfd74db93c986b5
Pheothenum <zero2004hr@gmail.com>
2016-06-13 02:43:09 +0200

    Removed few files Manifest refactored Added trim() on query for Search

--- app/src/main/java/hr/unipu/polyling/SearchActivity.java
+++ app/src/main/java/hr/unipu/polyling/SearchActivity.java
@@ -4,8 +4,6 @@ import android.app.ListActivity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.widget.ListView;
 
 import java.util.List;
 
@@ -34,7 +32,7 @@ public class SearchActivity extends ListActivity {
     }
 
     private void pretrazi(String query) {
-        fraze = baza.searchFraze(query);
+        fraze = baza.searchFraze(query.trim());
         setListAdapter(new FrazeArrayAdapter(this, R.layout.fraza_item, fraze));
     }
 
